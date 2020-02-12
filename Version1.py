b4e51785b278f32954124b5eab0234cf2428d8a5
nhossain819 <43657615+nhossain819@users.noreply.github.com>
2020-02-11 22:46:49 -0500

    Update and rename Version.1 to Version1.py

--- Version1.py
+++ Version1.py
@@ -1,15 +1,8 @@
-
 """
 DESCRIPTION:
-2/11/20 ONGOING PROJECT
-
 The following program takes in a sudoku board in terms of its rows and returns a completed sudoku puzzle.
 """
-
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-#PUZZLE INPUT AND ROW, COLUMN, SQUARE ORGANIZATION
-
-#Input the sudoku puzzle here using zeros instead of blanks.
+#Input the sudoku board here using zeros instead of blanks.
 row1 = [0 , 0 , 0 , 2 , 6 , 0 , 7 , 0 , 1]
 row2 = [6 , 8 , 0 , 0 , 7 , 0 , 0 , 9 , 0]
 row3 = [1 , 9 , 0 , 0 , 0 , 4 , 5 , 0 , 0]
@@ -20,7 +13,8 @@ row7 = [0 , 0 , 9 , 3 , 0 , 0 , 0 , 7 , 4]
 row8 = [0 , 4 , 0 , 0 , 5 , 0 , 0 , 3 , 6]
 row9 = [7 , 0 , 3 , 0 , 1 , 8 , 0 , 0 , 0]
 
-#The function show_board prints the input sudoku puzzle.
+
+#The function show_board prints the sudoku puzzle.
 def show_board():
     print(row1)
     print(row2)
@@ -32,13 +26,19 @@ def show_board():
     print(row8)
     print(row9)
 
+
+#sudoku_solver is a function with nested functions that inserts missing values when there is only one possible value per box.
+#sudoku_solver is run numerous times until all boxes are filled.
 def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9):
 
-    #all_rows is a single list of all rows squentially.
+    #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    #ROW, COLUMN, AND SQUARE VARIABLE DECLARATION
+
+    #all_rows is a list of all boxes of all rows.
     all_rows = list(row1 + row2 + row3 + row4 + row5 + row6 + row7 + row8 + row9)
 
 
-    #The function 'columns' creates a list of lists containing all columns of the input sudoku puzzle sequentially.
+    #The function 'columns' creates a list of lists containing all columns sequentially.
     def columns(all_rows_list , column_number):
         column = []
         column.append(all_rows_list[column_number - 1])
@@ -53,7 +53,7 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
         return column
 
 
-    #The variables column1-column9 contain the columns of the puzzle accordingly.
+    #The variables column1-column9 contain the columns of the board accordingly.
     column1 = columns(all_rows , 1)
     column2 = columns(all_rows , 2)
     column3 = columns(all_rows , 3)
@@ -65,7 +65,7 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
     column9 = columns(all_rows , 9)
 
 
-    #The variables square1-square9 contain the three-by-three- squares of the puzzle.
+    #The variables square1-square9 contain the three-by-three- squares of the board.
 
     #Square1: top row, left
     square1 = [row1[0], row1[1], row1[2], row2[0], row2[1], row2[2], row3[0], row3[1], row3[2]]
@@ -73,14 +73,14 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
     square2 = [row1[3], row1[4], row1[5], row2[3], row2[4], row2[5], row3[3], row3[4], row3[5]]
     #Square3: top row, right
     square3 = [row1[6], row1[7], row1[8], row2[6], row2[7], row2[8], row3[6], row3[7], row3[8]]
-    
+
     #Square4: middle row, left
     square4 = [row4[0], row4[1], row4[2], row5[0], row5[1], row5[2], row6[0], row6[1], row6[2]]
     #Square5: middle row, middle
     square5 = [row4[3], row4[4], row4[5], row5[3], row5[4], row5[5], row6[3], row6[4], row6[5]]
     #Square6: middle row, right
     square6 = [row4[6], row4[7], row4[8], row5[6], row5[7], row5[8], row6[6], row6[7], row6[8]]
-    
+
     #Square7: bottom row, left
     square7 = [row7[0], row7[1], row7[2], row8[0], row8[1], row8[2], row9[0], row9[1], row9[2]]
     #Square8: bottom row, middle
@@ -89,12 +89,11 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
     square9 = [row7[6], row7[7], row7[8], row8[6], row8[7], row8[8], row9[6], row9[7], row9[8]]
 
 
-    #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     #ANALYSIS
 
     #The function 'possible_values_by_row_column_square' provides all possible values for a single box based on its row, column, and square.
     def possible_values_by_row_column_square(row, column, square):
-
         row_value_list = []
         for row_element in row:
             if row_element == 0:
@@ -108,7 +107,6 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
                 continue
             else:
                 row_possible_value_list.append(element)
-
         column_value_list = []
         for column_element in column:
             if column_element == 0:
@@ -122,7 +120,6 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
                 continue
             else:
                 column_possible_value_list.append(element)
-
         square_value_list = []
         for square_element in square:
             if square_element == 0:
@@ -136,11 +133,10 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
                 continue
             else:
                 square_possible_value_list.append(element)
-
         final_framelist = list(range(1,10))
         final_possible_value_list = []
         for element in final_framelist:
-            if element in row_possible_value_list and element in column_possible_value_list and element in square_possible_value_list:
+            if (element in row_possible_value_list) and (element in column_possible_value_list) and (element in square_possible_value_list):
                 final_possible_value_list.append(element)
             else:
                 continue
@@ -247,6 +243,7 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
     possible_values_box80 = possible_values_by_row_column_square(row9, column8, square9)
     possible_values_box81 = possible_values_by_row_column_square(row9, column9, square9)
 
+    #All possible values per row.
     row1_possible_values = [possible_values_box1, possible_values_box2, possible_values_box3, possible_values_box4, possible_values_box5, possible_values_box6, possible_values_box7, possible_values_box8, possible_values_box9]
     row2_possible_values = [possible_values_box10, possible_values_box11, possible_values_box12, possible_values_box13, possible_values_box14, possible_values_box15, possible_values_box16, possible_values_box17, possible_values_box18]
     row3_possible_values = [possible_values_box19, possible_values_box20, possible_values_box21, possible_values_box22, possible_values_box23, possible_values_box24, possible_values_box25, possible_values_box26, possible_values_box27]
@@ -257,6 +254,7 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
     row8_possible_values = [possible_values_box64, possible_values_box65, possible_values_box66, possible_values_box67, possible_values_box68, possible_values_box69, possible_values_box70, possible_values_box71, possible_values_box72 ]
     row9_possible_values = [possible_values_box73, possible_values_box74, possible_values_box75, possible_values_box76, possible_values_box77, possible_values_box78, possible_values_box79, possible_values_box80, possible_values_box81]
 
+    #solve_rows replaces missing values in each row with the correct value.
     def solve_rows():
         framelist = list(range(9))
         for i in framelist:
@@ -279,16 +277,16 @@ def sudoku_solver(row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9)
             if (row9[i] == 0) and (len(row9_possible_values[i]) == 1):
                 row9[i] = row9_possible_values[i][0]
     solve_rows()
+#The end of the function sudoku_solver
 
-
-
-
+#Prints the original board for comparison
 print('Original Board')
 show_board()
 print(' ')
 
-framelist = list(range(10))
-for i in framelist:
+#Run the sudoku_solver function until
+runs_until_solved = list(range(10))
+for run in runs_until_solved:
     sudoku_solver(row1, row2, row3, row4, row5, row6, row7, row8, row9)
 
 print('Solved Board')
